f48332cbb1d677568ed71d7b469fe65ceaa74a3c
Rodrigo Peroza <rodrigoperoza@hotmail.com.br>
2019-07-17 14:57:47 -0300

    Pirmeiro Commit

---
+++ src/SGCLI.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGCLI.ApplicationCore.Entity
+{
+    class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int ClienteId { get; set; }
+        public int Nome { get; set; }
+        public int CPF { get; set; }
+    }
+}
